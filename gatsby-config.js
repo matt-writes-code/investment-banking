e9b8a871a54a291c87fbcf172bae17283c6b1c50
Matthew Yong <37174946+matt-writes-code@users.noreply.github.com>
2019-04-09 15:02:16 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Investment Banking`,
+    description: `A summary of investment banking terms and definitions.`,
     author: `@gatsbyjs`,
   },
   plugins: [
