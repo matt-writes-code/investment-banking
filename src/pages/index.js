9739fbe47e2c5364f41607776ecf24fac235ea56
Matthew Yong <37174946+matt-writes-code@users.noreply.github.com>
2019-04-11 12:59:42 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import FT from '../components/ft'
+import WSJ from '../components/wsj'
 import CNBC from '../components/cnbc'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Market News</h1>
-  <FT/>  
+  <FT/>
+  <WSJ/>
   <CNBC />
   </Layout>
 )
