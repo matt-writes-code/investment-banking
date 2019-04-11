bc69bb5c749ab32332bcc1808f1dc8937e71ad93
Matthew Yong <37174946+matt-writes-code@users.noreply.github.com>
2019-04-11 12:51:47 +0800

    Create ft.js

---
+++ src/components/ft.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import style from "./newsapi.module.css";
+
+const apikey = "7aeb97081f4140d6a65697a01b4c8d95";
+
+const news = items =>
+  `https://newsapi.org/v2/top-headlines?sources=financial-times&apiKey=${apikey}
+`;
+
+const News = props => (
+  <a className={style.newsholder} href={props.url}>
+    <div>
+      <img src={props.image} className={style.img} />
+    </div>
+    <div className={style.text}>
+      <div className={style.none}>
+        <b>{props.title}</b>
+      </div>
+      <div>{props.description}</div>
+    </div>
+  </a>
+);
+
+class FT extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch(news(this.props))
+      .then(d => d.json())
+      .then(d => {
+        this.setState({
+          data: d
+        });
+      });
+  }
+
+  render() {
+    if (!this.state.data) return <p>...</p>;
+    return (
+      <div>
+        <h3>CNBC</h3>
+        <div>
+          {this.state.data.articles.slice(0, 3).map(x => (
+            <News
+              image={x.urlToImage}
+              title={x.title}
+              description={x.description}
+              url={x.url}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FT;
