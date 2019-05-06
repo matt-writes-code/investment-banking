5a7b0b0d45920e778dbb9da8e9cd8462d4fac597
Matthew Yong <37174946+matt-writes-code@users.noreply.github.com>
2019-05-06 14:30:15 +0800

    Create time.js

---
+++ src/components/time.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  // Mounting: Set up a timer when clock is rendered to the DOM for the first time.
+  // ... and use tick() to tick once a second.
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  // Unmounting: Remove the timer whenever the Clock is removed from the DOM.
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  // Run the Clock component every second.
+  // Oh, and tell this.state.date to update itself whenever I change.
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div className="count-one">
+        <h1>Time</h1>
+        <p>State and Lifecycle</p>
+        <p>The time is now {this.state.date.toLocaleTimeString()}.</p>
+      </div>
+    );
+  }
+}
